46558e46dfdd90d51b89217666a0494635cba186
pallaviBorawake <pallavi2748.pb@gmail.com>
2023-06-26 01:21:24 +0900

    minor fix in sql file

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,5 +1,5 @@
 INSERT INTO UserData (id, username, password,role) VALUES
-  (1,'axaUser','password','ROLE_USER,ROLE_ADMIN'),
+  (1,'axaUser','password','ROLE_ADMIN'),
   (2,'otherAxa', 'pass123','ROLE_USER');
 
 
